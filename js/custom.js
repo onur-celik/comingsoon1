e72e068cd33b3f61f9bceb76ec776ca27daa8de8
Mustafa Onur Celik <onur.celik@logsign.net>
2016-08-03 17:37:07 +0300

    menu added font added

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,46 @@
 
 /* navigation bar */
-console.log("dasad");
 $(".nav-menu-link").click(function () {
-   	$(".menu-items").slideToggle();
-   	console.log("hilu");
+    $(".menu-items").slideToggle();
+    console.log("hilu");
 });
 
+
+jQuery(document).ready(function(){
+    if( $('.cd-stretchy-nav').length > 0 ) {
+        var stretchyNavs = $('.cd-stretchy-nav');
+        
+        stretchyNavs.each(function(){
+            var stretchyNav = $(this),
+                stretchyNavTrigger = stretchyNav.find('.cd-nav-trigger');
+            
+            stretchyNavTrigger.on('click', function(event){
+                event.preventDefault();
+                stretchyNav.toggleClass('nav-is-visible');
+            });
+        });
+
+        $(document).on('click', function(event){
+            ( !$(event.target).is('.cd-nav-trigger') && !$(event.target).is('.cd-nav-trigger span') ) && stretchyNavs.removeClass('nav-is-visible');
+        });
+    }
+
+    $(".cd-stretchy-nav > ul > li > a").click(function(e) { 
+        var id = $(this).attr("scrollTo");
+        e.preventDefault(); 
+        goToByScroll(id);
+    });
+});
+
+function goToByScroll(id){
+      // Remove "link" from the ID
+    console.log(id);
+    if (id != "home")
+    {
+        $('html,body').animate({ scrollTop: $("#"+id).offset().top}, 'slow');
+    }
+    else
+    {
+        $('html,body').animate({ scrollTop: 0}, 'slow');
+    }
+}
